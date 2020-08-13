9e5978f229ce728ab1b1aa8e5a23c13a810a05fc
Olivier St-Amand <olivier.stamand@unity3d.com>
2020-08-12 20:14:18 -0400

    Add mapping to one hot

--- tabnet/datasets/covertype.py
+++ tabnet/datasets/covertype.py
@@ -12,7 +12,7 @@ def get_data(path_to_csv, random_seed: int = RANDOM_SEED) -> Tuple[pd.DataFrame]
     df = pd.read_csv(path_to_csv)
     df_tr, df_test = train_test_split(df, test_size=0.2, random_state=random_seed)
     df_tr, df_val = train_test_split(
-        df_tr, test_size=0.2 / 0.8, random_state=random_seed
+        df_tr, test_size=0.2 / 0.6, random_state=random_seed
     )
     return df_tr, df_val, df_test
 
@@ -21,8 +21,13 @@ def build_dataset(
     df: pd.DataFrame,
     shuffle: bool = False,
     batch_size: int = 16384,
-    drop_remainder: bool = False,
+    drop_remainder: bool = True,
 ) -> tf.data.Dataset:
+    @tf.function
+    def prepare_data(x, y):
+        y = tf.one_hot(y - 1, 7)
+        return x, y
+
     # last column is label
     x = df[df.columns[:-1]].values.astype(np.float32)
     y = df[df.columns[-1]].values
@@ -30,5 +35,6 @@ def build_dataset(
     ds: tf.data.Dataset = tf.data.Dataset.from_tensor_slices((x, y))
     if shuffle:
         ds = ds.shuffle(buffer_size=len(x))
-    ds = ds.batch(batch_size, drop_remainder=drop_remainder).repeat()
+    ds = ds.batch(batch_size, drop_remainder=drop_remainder)
+    ds = ds.map(prepare_data).repeat()
     return ds
