94c66db9dd19da7a0452e5a5216765f92e480e4c
Olivier St-Amand <olivier.st.amand.1@gmail.com>
2020-08-22 19:57:56 -0400

    Add custom bn

--- tabnet/models/gbn.py
+++ tabnet/models/gbn.py
@@ -3,18 +3,19 @@ import tensorflow as tf
 
 class GhostBatchNormalization(tf.keras.Model):
     def __init__(
-        self, virtual_divider: int = 128, momentum: float = 0.9, epsilon: float = 1e-5
+        self, virtual_divider: int = 1, momentum: float = 0.9, epsilon: float = 1e-5
     ):
         super(GhostBatchNormalization, self).__init__()
         self.virtual_divider = virtual_divider
-        self.bn = tf.keras.layers.BatchNormalization(momentum=momentum, epsilon=epsilon)
+        #self.bn = tf.keras.layers.BatchNormalization(momentum=momentum, epsilon=epsilon)
+        self.bn = BatchNormInferenceWeighting(momentum=momentum)
 
-    def call(self, x, training=None):
+    def call(self, x, training: bool = None, alpha: float = 0.0):
         if training:
             chunks = tf.split(x, self.virtual_divider)
             x = [self.bn(x, training=True) for x in chunks]
             return tf.concat(x, 0)
-        return self.bn(x, training=False)
+        return self.bn(x, training=False, alpha=alpha)
 
     @property
     def moving_mean(self):
@@ -23,3 +24,46 @@ class GhostBatchNormalization(tf.keras.Model):
     @property
     def moving_variance(self):
         return self.bn.moving_variance
+
+
+class BatchNormInferenceWeighting(tf.keras.layers.Layer):
+    def __init__(self, momentum: float = 0.9, epsilon: float = None):
+        super(BatchNormInferenceWeighting, self).__init__()
+        self.momentum = momentum
+        self.epsilon = tf.keras.backend.epsilon() if epsilon is None else epsilon
+
+    def build(self, input_shape):
+        channels = input_shape[-1]
+
+        self.gamma = tf.Variable(initial_value=tf.ones((channels,), tf.float32), trainable=True,)
+        self.beta = tf.Variable(initial_value=tf.zeros((channels,), tf.float32), trainable=True,)
+
+        self.moving_mean = tf.Variable(initial_value=tf.zeros((channels,), tf.float32), trainable=False,)
+        self.moving_mean_of_squares = tf.Variable(initial_value=tf.zeros((channels,), tf.float32), trainable=False,)
+
+    def __update_moving(self, var, value):
+        var.assign(var * self.momentum + (1-self.momentum) * value)
+
+    def __apply_normalization(self, x, mean, variance):
+        return self.gamma * (x - mean) / tf.sqrt(variance + self.epsilon) + self.beta
+
+    def call(self, x, training: bool = None, alpha: float = 0.0):
+        mean = tf.reduce_mean(x, axis=0)
+        mean_of_squares = tf.reduce_mean(tf.pow(x,2), axis=0)
+
+        if training:
+            # update moving stats
+            self.__update_moving(self.moving_mean, mean)
+            self.__update_moving(self.moving_mean_of_squares, mean_of_squares)
+
+            variance = mean_of_squares - tf.pow(mean, 2)
+            x = self.__apply_normalization(x, mean, variance)
+        else:
+            mean = alpha * mean + (1-alpha) * self.moving_mean
+            variance = (alpha * mean_of_squares + (1-alpha) * self.moving_mean_of_squares) - tf.pow(mean, 2)
+            x = self.__apply_normalization(x, mean, variance)
+
+        return x
+
+
+
---
+++ tests/test_custom_bn.py
@@ -0,0 +1,46 @@
+import pytest
+import tensorflow as tf
+
+from tabnet.models.gbn import BatchNormInferenceWeighting
+
+
+class CustomBatchNormTest(tf.test.TestCase):
+
+    def setUp(self):
+        self.x = tf.random.uniform(shape=(32,54), dtype=tf.float32)
+        self.zeros = tf.zeros(self.x.shape[1])
+        self.ones = tf.ones(self.x.shape[1])
+
+    def test_can_apply_bn_in_training(self):
+        bn = BatchNormInferenceWeighting()
+        x_bn = bn(self.x, training=True)
+
+        mean = tf.reduce_mean(x_bn, axis=0)
+        std = tf.sqrt(tf.reduce_mean(tf.pow(x_bn, 2), axis=0) - tf.pow(tf.reduce_mean(x_bn, axis=0), 2))
+
+        self.assertAllClose(mean, self.zeros)
+        self.assertAllClose(std, self.ones)
+
+    def test_update_moving_stats_only_in_training(self):
+        bn = BatchNormInferenceWeighting()
+        _ = bn(self.x, training=False)
+
+        self.assertAllClose(bn.moving_mean, self.zeros)
+        self.assertAllClose(bn.moving_mean_of_squares, self.zeros)
+
+        _ = bn(self.x, training=True)
+
+        self.assertNotAllClose(bn.moving_mean, self.zeros)
+        self.assertNotAllClose(bn.moving_mean_of_squares, self.zeros)
+
+    def test_similar_to_keras(self):
+        bn = BatchNormInferenceWeighting(momentum=0.9)
+        bn_keras = tf.keras.layers.BatchNormalization(momentum=0.9, epsilon=tf.keras.backend.epsilon())
+
+        x_bn = bn(self.x, training=True)
+        x_bn_keras = bn_keras(self.x, training=True)
+
+        self.assertAllClose(x_bn, x_bn_keras)
+        #TODO check moving mean & std
+
+tf.test.main()
