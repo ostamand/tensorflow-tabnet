a5c92c27cf829ecc5a4e69fac5cc219adff11c4e
Olivier St-Amand <olivier.st.amand.1@gmail.com>
2020-08-24 14:38:59 -0400

    Add find_packages

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 REQUIREMENTS = ["tensorflow>=2.0.0", "tensorflow-addons>=0.11.1"]
 
@@ -9,7 +9,7 @@ setup(
     author_email="olivier.st.amand.1@gmail.com",
     description="TensorFlow 2 implementation of TabNet",
     license="MIT",
-    packages=["tabnet"],
+    packages=find_packages(exclude=["tests"]),
     python_requires=">=3.5.0",
     install_requires=REQUIREMENTS,
 )
